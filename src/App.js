6c9da28c820eb20b9065aa748c785c739ad8013e
773948680 <alalpha394@gmail.com>
2025-05-08 18:11:39 +0000

    git add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import Router from './Router'
+import HomePage from './pages/HomePage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import LastProducts from './components/marketing/pageSections/LastProducts'
 // import Event from './components/marketing/pageSections/Event'
 // import Products from './components/marketing/pageSections/Products'
 
 export default function App () {
   return (
-    <div>
-        <Router />
+                <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
        
        
        
-    </div>
-  )
+  
 }
